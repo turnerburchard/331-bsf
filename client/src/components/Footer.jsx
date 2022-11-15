21ea2a19717486cb537508ce56c0c69bf8b209e0
brady.ash <j11q863@csci331.cs.montana.edu>
2022-11-15 12:09:27 -0700

    Changed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer>
         <h4>Hosted by: Brady Ash ~ j11q863 </h4>
-        <p> <b>Port:</b> 30/50xx </p>
+        <p> <b>Port:</b> 30/5065 </p>
         <p> Backend linking to a database </p>
     </footer>
   )
