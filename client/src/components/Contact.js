9e55703f860c35f6166ada244c58dd8f98342b59
Turner <tburchard4@gmail.com>
2022-11-30 14:29:32 -0700

    contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,7 +1,25 @@
 function Contact(){
     return(
         <div>
-            <h1 className={"rainbow"}>THIS PAGE IS UNDER CONSTRUCTION</h1>
+            <div style={{display: 'flex', alignSelf: 'center', flexDirection: 'column', marginLeft: '40%', width: '20%', marginRight: '40%', alignContent: 'center'}}><h1 >Contact us</h1>
+                <div>
+                    <div>
+                        <figure>
+                            <div style={{width: '500px'}}>
+                                <iframe allowFullScreen="true"
+                                        src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.google.com%2Fmaps%2Fembed%2Fv1%2Fplace%3Fcenter%3D45.703532%252C-111.025565%26key%3DAIzaSyD9HrlRuI1Ani0-MTZ7pvzxwxi4pgW0BCY%26zoom%3D17%26q%3DBridger%2BSki%2BFoundation&amp;display_name=Google+Maps&amp;url=https%3A%2F%2Fwww.google.com%2Fmaps%2Fplace%2FBridger%2BSki%2BFoundation%2F%4045.703532%2C-111.025565%2C17z%2Fdata%3D%214m5%213m4%211s0x0%3A0xcb217a68296dbe56%218m2%213d45.7034796%214d-111.0261014%3Fshorturl%3D1&amp;image=http%3A%2F%2Fmaps-api-ssl.google.com%2Fmaps%2Fapi%2Fstaticmap%3Fcenter%3D45.703532%2C-111.025565%26zoom%3D15%26size%3D250x250%26sensor%3Dfalse&amp;key=96f1f04c5f4143bcb0f2e68c87d65feb&amp;type=text%2Fhtml&amp;schema=google"></iframe>
+                            </div>
+                        </figure>
+                        <p><strong>Street Address:&nbsp;</strong>&nbsp;<a
+                            href="https://goo.gl/maps/Z69CmcHkBpNbUXBy8"><strong>2339 Birdie Drive, Bozeman, MT
+                            59715</strong></a>.</p><p><strong>Mailing Address:</strong>&nbsp; P.O. Box 1243, Bozeman MT
+                        59771</p><p><strong>Telephone:</strong>&nbsp; 406.587.2445 </p><p>
+                        <strong>Email:</strong>&nbsp;admin@bridgerskifoundation.org<strong>&nbsp;</strong></p><p>‍</p>
+                        <h3>OFFICE HOURS</h3><p><strong>Sept-May:</strong>&nbsp;&nbsp;10:00 a.m. to 3:00 p.m.,
+                        Monday-Thursday</p><p><strong>June - Aug:</strong>&nbsp;10 a.m. - 1:00 p.m., Tues, Wed, Thurs.
+                    </p><p>‍</p></div>
+                </div>
+            </div>
         </div>
     )
 }
