d26b5697ec4e43ea88c5c02b1cc89435c8a0e4ce
Brady Ash <bradyash9@gmail.com>
2022-12-05 09:15:41 -0700

    wth am i doin

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -15,8 +15,8 @@ function App() {
       <hr />
       <CurrentEntries />
       <hr />
-        <CsvDownload />
-        <hr />
+      <CsvDownload />
+      <hr />
       <Footer />
     </div>
   )
