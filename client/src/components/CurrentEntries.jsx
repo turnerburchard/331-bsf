c527e21bd405b12e1d5c9ff03c608294d009da3b
Connor Parrott <connorparrott123@gmail.com>
2022-12-12 12:49:32 -0700

    nothing to change here

--- client/src/components/CurrentEntries.jsx
+++ client/src/components/CurrentEntries.jsx
@@ -65,9 +65,10 @@ const CurrentEntries = () => {
 
   const sendEmail = () => {
     alert('processing')
+    
     var templateParams = {
-      name: 'James',
-      notes: 'Check this out!'
+      to_name: 'BSF Dummy',
+      from_name: 'Bridger Ski Foundation'
     };
     emailjs.send('service_u979zkh', 'template_f6sg1oh', templateParams) //use your Service ID and Template ID
       .then(function(response) {
